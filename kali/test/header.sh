31b6017d232be750f570949d566a5fdc5110266b
Apathetic Euphoria <ApatheticEuphoria@users.noreply.github.com>
2016-03-12 13:27:45 -0800

    Update header.sh
    Fixed

--- kali/test/header.sh
+++ kali/test/header.sh
@@ -2,8 +2,8 @@ function mostrarheader(){
 	
 	conditional_clear
 	echo -e "$blue#########################################################"
-	echo -e "$blue#	$red    FLUXION $version" "${yellow} ${red} F""${yellow}lux" "${red}I""${yellow}s" "${red}T""${yellow}he ""${red}F""${yellow}uture"              ${blue}"     #"        
-	echo -e "$blue#"${yellow} by "${rot}D""${yellow}eltax", "${red}"S""${yellow}"trasharo and "${red}A""${yellow}patheticEuphoria""            ${blue}#""      
+	echo -e "$blue#	$red    FLUXION $version" "${yellow} ${red} F""${yellow}lux" "${red}I""${yellow}s" "${red}T""${yellow}he ""${red}F""${yellow}uture         "              ${blue}"     #"        
+	echo -e "$blue#"${yellow} by "${red}D""${yellow}eltax", "${red}"S""${yellow}"trasharo and "${red}A""${yellow}patheticEuphoria"           "    ${blue}#""      
 	echo -e "$blue#                                                       #"
 	echo -e "$blue#########################################################""$transparent"
 	echo
