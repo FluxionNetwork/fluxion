2f339dcf2786e2c7433b70f8de5cbadcb2fb2e8f
deltax <deltax.fluxion@gmail.com>
2016-05-06 14:49:31 +0200

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,205 +1,175 @@
-##Only for Kali 
+#!/bin/bash
 
-####### < Color > #######
-white="\033[1;37m"
-grey="\033[0;37m"
-red2="\033[0;35m"
-red="\033[1;31m"
-green="\033[1;32m"
-yellow="\033[1;33m"
-blue="\033[1;34m"
-transparent="\e[0m"
-#########################
+####################################
+##			      				  ##
+## Script for install dependences ##
+##			      				  ##
+####################################
+echo "Updating system..."
+sudo apt-get --yes update > /dev/null 2>&1 
+##############################
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root." 1>&2
-   exit 1
-fi
-
-############################################# < START >	####################################################
-	
-	sudo apt-get update 
-	
-	clear
-	echo -e "\e[1;31mInstaller - by Deltax"
-	echo -e "\e[1;31mCheck & autoinstall"
-	echo -e "\e[1;31mDebian only"
-	echo -e "\e[1;31mFix coming soon"
-	echo -e "\e[1;31mBug add-apt-repository"
-	echo -e "\e[1;31mor add kali sources in etc/apt/source.lst"
-
-	clear
-	echo -ne "Aircrack-ng....."
+echo -ne "Aircrack-ng....."
 	if ! hash aircrack-ng 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		 apt-get install aircrack-ng -y
+		echo " Installing ..."
+		sudo apt-get --yes install aircrack-ng > /dev/null 2>&1 
 	else
-		echo -e "\e[1;32mOK!"$transparent""
+		echo -e "! ok"
 	fi
 	sleep 0.025
-	
-	echo -ne "Aireplay-ng....."
-	if ! hash aireplay-ng 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install aireplay-ng -y
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
-	
-	echo -ne "Airmon-ng......."
-	if ! hash airmon-ng 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install aircrack-ng -y
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
-	
-	echo -ne "Airodump-ng....."
-	if ! hash airodump-ng 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install aircrack-ng -y
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
-	
-	echo -ne "Awk............."
+
+##############################
+
+echo -ne "Awk....."
 	if ! hash awk 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install awk -y
+		echo " Installing ..."
+		sudo apt-get --yes install gawk > /dev/null 2>&1 
 	else
-		echo -e "\e[1;32mOK!"$transparent""
+		echo -e "! ok"
 	fi
 	sleep 0.025
-	
-	echo -ne "Curl............"
+##############################
+
+echo -ne "Curl....."
 	if ! hash curl 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install curl -y
+		echo " Installing ..."
+		sudo apt-get --yes install curl > /dev/null 2>&1 
 	else
-		echo -e "\e[1;32mOK!"$transparent""
+		echo -e " ! ok"
 	fi
 	sleep 0.025
-	
-	echo -ne "Dhcpd..........."
+##############################
+
+echo -ne "Dhcpd....."
 	if ! hash dhcpd 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent" (isc-dhcp-server)"
-		apt-get install isc-dhcp-server -y
+		echo " Installing ..."
+		sudo apt-get --yes install isc-dhcp-server > /dev/null 2>&1 
 	else
-		echo -e "\e[1;32mOK!"$transparent""
+		echo -e " ! ok"
 	fi
 	sleep 0.025
-	
-	echo -ne "Hostapd........."
+##############################
+
+echo -ne "Hostapd....."
 	if ! hash hostapd 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install hostapd -y
+		echo " Installing ..."
+		sudo apt-get --yes install hostapd > /dev/null 2>&1 
 	else
-		echo -e "\e[1;32mOK!"$transparent""
+		echo -e " ! ok"
 	fi
 	sleep 0.025
-	
-	echo -ne "Iwconfig........"
-	if ! hash iwconfig 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install wireless-tools -y
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
-	
-	echo -ne "Lighttpd........"
+##############################
+
+echo -ne "Lighttpd....."
 	if ! hash lighttpd 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install lighttpd -y
+		echo " Installing ..."
+		sudo apt-get --yes install lighttpd > /dev/null 2>&1 
 	else
-		echo -e "\e[1;32mOK!"$transparent""
+		echo -e " ! ok"
 	fi
 	sleep 0.025
-	
-	echo -ne "Macchanger......"
+##############################
+
+echo -ne "Macchanger....."
 	if ! hash macchanger 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install macchanger -y
+		echo " Installing ..."
+		sudo apt-get --yes install macchanger > /dev/null 2>&1 
 	else
-	    echo -e "\e[1;32mOK!"$transparent""
+		echo -e " ! ok"
 	fi
 	sleep 0.025
-	
-	echo -ne "Mdk3............"
-	if ! hash mdk3 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install mdk3 -y
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
-	
-	echo -ne "Php5-cgi........"
-	if ! [ -f /usr/bin/php-cgi ]; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install php5-cgi -y
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
-	
-	echo -ne "Pyrit..........."
-	if ! hash pyrit 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install pyrit
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
-	
-	echo -ne "Python.........."
-	if ! hash python 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install python -y
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
-	
-	echo -ne "Unzip..........."
-	if ! hash unzip 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install unzip -y
-	else
-		echo -e "\e[1;32mOK!"$transparent""
-	fi
-	sleep 0.025
+##############################
 
-	echo -ne "Mdk3............"
+echo -ne "Mdk3....."
 	if ! hash mdk3 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		cd 
-		wget http://linux.gungoos.com/mdk3-v6.tar.gz
-		tar xvzf mdk3-v6.tar.gz
+		echo " Installing ..."
+		wget https://raw.githubusercontent.com/Wikelx/mdk3-v6/master/mdk3-v6.tar.bz2
+		tar -vxjf mdk3-v6.tar.bz2
 		cd mdk3-v6
-		apt-get install patch
-		wget http://linux.gungoos.com/mdk3-v6.patch
-		patch Makefile < mdk3-v6.patch
-		rm mdk3-v6.patch
-		make
-		make install	
+		sudo make 
+		sudo make install
+		cd .. 
+		sudo rm -r mdk3-v6.tar.bz2
+		sudo rm -r mdk3-v6
+		
 	else
-		echo -e "\e[1;32mOK!"$transparent""
+		echo -e " ! ok"
 	fi
 	sleep 0.025
+##############################
 
-	echo -ne "Xterm..........."
+echo -ne "Nmap....."
+	if ! hash nmap 2>/dev/null; then
+		echo " Installing ..."
+		sudo apt-add-repository ppa:pi-rho/security -y  #ppa Nmap
+		sudo apt-get --yes update  > /dev/null 2>&1 
+		sudo apt-get install nmap  > /dev/null 2>&1 
+	else
+		echo -e " ! ok"
+	fi
+	sleep 0.025
+##############################
+
+echo -ne "Php5-cgi....."
+	if ! hash php-cgi 2>/dev/null; then
+		echo " Installing ..."
+		sudo apt-get --yes install php5-cgi > /dev/null 2>&1 
+	else
+		echo -e " ! ok"
+	fi
+	sleep 0.025
+##############################
+
+echo -ne "Pyrit....."
+	if ! hash pyrit 2>/dev/null; then
+		echo " Installing ..."
+		sudo apt-get --yes install pyrit > /dev/null 2>&1 
+	else
+		echo -e " ! ok"
+	fi
+	sleep 0.025
+##############################
+
+echo -ne "Python....."
+	if ! hash python 2>/dev/null; then
+		echo " Installing ..."
+		sudo apt-get install libssl-dev openssl > /dev/null 2>&1 
+		cd /opt
+		sudo wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tar.xz
+		sudo tar -Jxf Python-3.5.1.tar.xz  > /dev/null 2>&1 
+		cd Python-3.5.1
+		./configure
+		sudo make
+		sudo make install
+		cd ..
+		sudo rm -r Python-3.5.1.tar.xz > /dev/null 2>&1
+		sudo rm -r Python-3.4.1 > /dev/null 2>&1
+		sudo ln -fs /opt/Python-3.5.1/python /usr/bin/python > /dev/null 2>&1
+		cd ~/ > /dev/null 2>&1
+	else
+		echo -e " ! ok"
+	fi
+	sleep 0.025
+##############################
+
+echo -ne "Unzip....."
+	if ! hash unzip 2>/dev/null; then
+		echo " Installing ..."
+		sudo apt-get install unzip > /dev/null 2>&1
+	else
+		echo -e " ! ok"
+	fi
+	sleep 0.025
+##############################
+
+echo -ne "Xterm....."
 	if ! hash xterm 2>/dev/null; then
-		echo -e "\e[1;31mNot installed"$transparent""
-		apt-get install xterm -y
+		echo " Installing ..."
+		sudo apt-get install xterm > /dev/null 2>&1
 	else
-		echo -e "\e[1;32mOK!"$transparent""
+		echo -e " ! ok"
 	fi
 	sleep 0.025
-	sleep 10
-	clear
-
-
+##############################
+echo "Finish !"
+exit
