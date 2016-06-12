ae0da3f9c89cad7d4747fdba5ace9214ca516d70
deltax <deltax.fluxion@gmail.com>
2016-06-12 16:41:09 +0100

    Add files via upload

---
+++ remove.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+import sys, traceback
+
+infile = "/etc/apt/sources.list"
+outfile = "/etc/apt/sources.list"
+
+delete_list = ["# Kali linux repositories | Added by Katoolin\n", "deb http://http.kali.org/kali kali-rolling main contrib non-free\n","deb http://repo.kali.org/kali kali-bleeding-edge main\n"]
+fin = open(infile)
+os.remove("/etc/apt/sources.list")
+fout = open(outfile, "w+")
+for line in fin:
+    for word in delete_list:
+        line = line.replace(word, "")
+    fout.write(line)
+fin.close()
+fout.close()
+
+print "\033[1;31mDONE!  !\033[1;m"
