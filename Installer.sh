41b7ab9337ea266aa1cbaf0c60c116ef378d4d40
deltax <deltax.fluxion@gmail.com>
2016-06-12 16:40:40 +0100

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,175 +1,349 @@
 #!/bin/bash
 
-####################################
-##			      				  ##
-## Script for install dependences ##
-##			      				  ##
-####################################
+INSTALLER_DEBUG=1
+#Config
+version=2
+revision=3
+#Colors
+red='\e[1;31m'
+blue='\e[1;34m'
+yellow='\e[1;33m'
+transparent="\e[0m"
+
+#DUMP_PATH
+rm -rf /tmp/Installer/
+mkdir /tmp/Installer/
+DUMP_PATH="/tmp/Installer/"
+
+function conditional_clear() {
+
+	if [[ "$INSTALLER_output_device" != "/dev/stdout" ]]; then clear; fi
+}
+
+#Config_END
+if [ $INSTALLER_DEBUG = 1 ]; then
+	## Developer Mode
+	export INSTALLER_output_device=/dev/stdout
+	HOLD="-hold"
+else
+	## Normal Mode
+	export INSTALLER_output_device=/dev/null
+	HOLD=""
+fi
+
+#Check root
+if [[ $EUID -ne 0 ]]; then
+        echo -e "\e[1;31mYou don't have admin privilegies, execute the script as root."$transparent
+				exit
+fi
+
+clear
+
+
+function mostrarheader(){
+
+
+	echo -e "$blue#########################################################"
+	echo -e "$blue#                                                       #"
+  echo -e "$blue#  $red    FLUXION $version" "${yellow} ${red}  < F""${yellow}luxion" "${red}I""${yellow}s" "${red}T""${yellow}he ""${red}F""${yellow}uture >     "          ${blue}"  #"
+	echo -e "$blue#"${yellow} by "${red}D""${yellow}eltax", "${red}"S""${yellow}"trasharo and "${red}A""${yellow}patheticEuphoria"           "    ${blue}#""
+	echo -e "$blue#                                                       #"
+	echo -e "$blue#########################################################""$transparent"
+	echo
+	echo
+
+}
+
+function setresolution {
+
+	function resA {
+
+		TOPLEFT="-geometry 90x13+0+0"
+		TOPRIGHT="-geometry 83x26-0+0"
+		BOTTOMLEFT="-geometry 90x24+0-0"
+		BOTTOMRIGHT="-geometry 75x12-0-0"
+		TOPLEFTBIG="-geometry 91x42+0+0"
+		TOPRIGHTBIG="-geometry 83x26-0+0"
+	}
+
+	function resB {
+
+		TOPLEFT="-geometry 92x14+0+0"
+		TOPRIGHT="-geometry 68x25-0+0"
+		BOTTOMLEFT="-geometry 92x36+0-0"
+		BOTTOMRIGHT="-geometry 74x20-0-0"
+		TOPLEFTBIG="-geometry 100x52+0+0"
+		TOPRIGHTBIG="-geometry 74x30-0+0"
+	}
+	function resC {
+
+		TOPLEFT="-geometry 100x20+0+0"
+		TOPRIGHT="-geometry 109x20-0+0"
+		BOTTOMLEFT="-geometry 100x30+0-0"
+		BOTTOMRIGHT="-geometry 109x20-0-0"
+		TOPLEFTBIG="-geometry  100x52+0+0"
+		TOPRIGHTBIG="-geometry 109x30-0+0"
+	}
+	function resD {
+		TOPLEFT="-geometry 110x35+0+0"
+		TOPRIGHT="-geometry 99x40-0+0"
+		BOTTOMLEFT="-geometry 110x35+0-0"
+		BOTTOMRIGHT="-geometry 99x30-0-0"
+		TOPLEFTBIG="-geometry 110x72+0+0"
+		TOPRIGHTBIG="-geometry 99x40-0+0"
+	}
+	function resE {
+		TOPLEFT="-geometry 130x43+0+0"
+		TOPRIGHT="-geometry 68x25-0+0"
+		BOTTOMLEFT="-geometry 130x40+0-0"
+		BOTTOMRIGHT="-geometry 132x35-0-0"
+		TOPLEFTBIG="-geometry 130x85+0+0"
+		TOPRIGHTBIG="-geometry 132x48-0+0"
+	}
+	function resF {
+		TOPLEFT="-geometry 100x17+0+0"
+		TOPRIGHT="-geometry 90x27-0+0"
+		BOTTOMLEFT="-geometry 100x30+0-0"
+		BOTTOMRIGHT="-geometry 90x20-0-0"
+		TOPLEFTBIG="-geometry  100x70+0+0"
+		TOPRIGHTBIG="-geometry 90x27-0+0"
+}
+
+detectedresolution=$(xdpyinfo | grep -A 3 "screen #0" | grep dimensions | tr -s " " | cut -d" " -f 3)
+##  A) 1024x600
+##  B) 1024x768
+##  C) 1280x768
+##  D) 1280x1024
+##  E) 1600x1200
+case $detectedresolution in
+	"1024x600" ) resA ;;
+	"1024x768" ) resB ;;
+	"1280x768" ) resC ;;
+	"1366x768" ) resC ;;
+	"1280x1024" ) resD ;;
+	"1600x1200" ) resE ;;
+	"1366x768"  ) resF ;;
+		  * ) resA ;;
+esac
+}
+
+#Install Main
+conditional_clear
+mostrarheader
+
 echo "Updating system..."
-sudo apt-get --yes update > /dev/null 2>&1 
+xterm $HOLD -title "Updating System"  -e apt-get update
+xterm $HOLD -title "Updating System"  -e apt-get install
+xterm $HOLD -title "Updating System"  -e apt-get install software-properties-common
+xterm $HOLD -title "Updating System"  -e python remove.py
+xterm $HOLD -title "Updating System"  -e python add.py
+
 ##############################
 
 echo -ne "Aircrack-ng....."
 	if ! hash aircrack-ng 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install aircrack-ng > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent
+	 xterm $HOLD -title "Installing Aircrack-ng" -e apt-get --yes install aircrack-ng
 	else
-		echo -e "! ok"
+    echo -e "\e[1;32mOK!"$transparent
 	fi
 	sleep 0.025
 
 ##############################
 
-echo -ne "Awk....."
+echo -ne "Aireplay-ng....."
 	if ! hash awk 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install gawk > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+		xterm $HOLD -title "Installing Awk" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install gawk
 	else
-		echo -e "! ok"
+		echo -e "\e[1;32mOK!"$transparent
 	fi
 	sleep 0.025
 ##############################
 
-echo -ne "Curl....."
+echo -ne "Airodump-ng....."
+if ! hash airodump-ng 2>/dev/null; then
+	echo -e "\e[1;31mInstalling ..."$transparent""
+	xterm $HOLD -title "Installing Airodump-ng" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install aircrack-ng
+else
+	echo -e "\e[1;32mOK!"$transparent""
+fi
+##############################
+	echo -ne "Bully..........."
+	if ! hash bully 2>/dev/null; then
+		echo -e "\e[1;31mInstalling ..."$transparent""
+		xterm $HOLD -title "Installing Bully" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install bully
+	else
+		echo -e "\e[1;32mOK!"$transparent""
+	fi
+	sleep 0.025
+##############################
+echo -ne "Curl............"
 	if ! hash curl 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install curl > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+			xterm $HOLD -title "Installing Curl" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install curl
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  fi
 	sleep 0.025
 ##############################
 
-echo -ne "Dhcpd....."
+echo -ne "Dhcpd..........."
 	if ! hash dhcpd 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install isc-dhcp-server > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+			xterm $HOLD -title "Installing Dhcpd" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000"-e apt-get --yes install isc-dhcp-server
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  	fi
 	sleep 0.025
 ##############################
 
-echo -ne "Hostapd....."
+echo -ne "Hostapd........."
 	if ! hash hostapd 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install hostapd > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+		xterm $HOLD -title "Installing Hostapd" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install hostapd
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  	fi
 	sleep 0.025
 ##############################
 
-echo -ne "Lighttpd....."
+echo -ne "Iwconfig........"
+if ! hash iwconfig 2>/dev/null; then
+			echo -e "\e[1;31mInstalling ..."$transparent""
+			xterm $HOLD -title "Installing Iwconfig" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install iwconfig
+else
+	echo -e "\e[1;32mOK!"$transparent""
+fi
+sleep 0.025
+##############################
+	echo -ne "Lighttpd........"
 	if ! hash lighttpd 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install lighttpd > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+			xterm $HOLD -title "Installing Lighttpd" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install lighttpd
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  	fi
 	sleep 0.025
 ##############################
 
-echo -ne "Macchanger....."
+echo -ne "Macchanger......"
 	if ! hash macchanger 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install macchanger > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+			xterm $HOLD -title "Installing Macchanger" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install macchanger
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  fi
 	sleep 0.025
 ##############################
 
-echo -ne "Mdk3....."
+echo -ne "Mdk3............"
 	if ! hash mdk3 2>/dev/null; then
-		echo " Installing ..."
-		wget https://raw.githubusercontent.com/Wikelx/mdk3-v6/master/mdk3-v6.tar.bz2
-		tar -vxjf mdk3-v6.tar.bz2
-		cd mdk3-v6
-		sudo make 
-		sudo make install
-		cd .. 
-		sudo rm -r mdk3-v6.tar.bz2
-		sudo rm -r mdk3-v6
-		
+	echo -e "\e[1;31mInstalling ..."$transparent""
+	xterm $HOLD -title "Installing Macchanger" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install mdk3
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  fi
 	sleep 0.025
+
 ##############################
 
-echo -ne "Nmap....."
+echo -ne "Nmap............"
 	if ! hash nmap 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-add-repository ppa:pi-rho/security -y  #ppa Nmap
-		sudo apt-get --yes update  > /dev/null 2>&1 
-		sudo apt-get install nmap  > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+		xterm $HOLD -title "Installing Nmap" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get install --yes nmap
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  fi
 	sleep 0.025
 ##############################
-
-echo -ne "Php5-cgi....."
+echo -ne "Openssl........."
+if ! hash openssl 2>/tmp/null; then
+	echo -e "\e[1;31mInstalling ..."$transparent""
+	xterm $HOLD -title "Installing Openssl" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e openssl
+else
+	echo -e "\e[1;32mOK!"$transparent""
+fi
+sleep 0.025
+##############################
+echo -ne "Php5-cgi........"
 	if ! hash php-cgi 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install php5-cgi > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+			xterm $HOLD -title "Installing Php5-cgi" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install php5-cgi
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  fi
 	sleep 0.025
 ##############################
 
-echo -ne "Pyrit....."
+	echo -ne "Pyrit..........."
 	if ! hash pyrit 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get --yes install pyrit > /dev/null 2>&1 
+		echo -e "\e[1;31mInstalling ..."$transparent""
+		xterm $HOLD -title "Installing Pyrit" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get --yes install pyrit
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  fi
 	sleep 0.025
 ##############################
 
-echo -ne "Python....."
+echo -ne "Python.........."
 	if ! hash python 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get install libssl-dev openssl > /dev/null 2>&1 
-		cd /opt
-		sudo wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tar.xz
-		sudo tar -Jxf Python-3.5.1.tar.xz  > /dev/null 2>&1 
-		cd Python-3.5.1
-		./configure
-		sudo make
-		sudo make install
-		cd ..
-		sudo rm -r Python-3.5.1.tar.xz > /dev/null 2>&1
-		sudo rm -r Python-3.4.1 > /dev/null 2>&1
-		sudo ln -fs /opt/Python-3.5.1/python /usr/bin/python > /dev/null 2>&1
-		cd ~/ > /dev/null 2>&1
+		echo -e "\e[1;31mInstalling ..."$transparent""
+			xterm $HOLD -title "Installing Python" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get install python
 	else
-		echo -e " ! ok"
+		echo -e "\e[1;32mOK!"$transparent
+  fi
+	sleep 0.025
+
+##############################
+	echo -ne "Reaver.........."
+	if ! hash reaver 2>/dev/null; then
+		echo -e "\e[1;31mInstalling ..."$transparent""
+		xterm $HOLD -title "Installing Reaver" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get install reaver
+	else
+		echo -e "\e[1;32mOK!"$transparent""
 	fi
 	sleep 0.025
 ##############################
 
-echo -ne "Unzip....."
+	echo -ne "rfkill.........."
+        if ! hash rfkill 2>/dev/null; then
+              echo -e "\e[1;31mInstalling ..."$transparent""
+              xterm $HOLD -title "Installing Rfkill" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get install rfkill
+        else
+                echo -e "\e[1;32mOK!"$transparent""
+        fi
+        sleep 0.025
+
+##############################
+
+echo -ne "Unzip..........."
 	if ! hash unzip 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get install unzip > /dev/null 2>&1
+		echo -e "\e[1;31mInstalling ..."$transparent""
+			xterm $HOLD -title "Installing unzip" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get install unzip
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  fi
 	sleep 0.025
 ##############################
 
-echo -ne "Xterm....."
+echo -ne "Xterm..........."
 	if ! hash xterm 2>/dev/null; then
-		echo " Installing ..."
-		sudo apt-get install xterm > /dev/null 2>&1
+		echo -e "\e[1;31mInstalling ..."$transparent""
+		apt-get install xterm
 	else
-		echo -e " ! ok"
-	fi
+		echo -e "\e[1;32mOK!"$transparent
+  fi
 	sleep 0.025
 ##############################
-echo "Finish !"
-exit
+echo -ne "Zenity.........."
+if ! hash zenity 2>/tmp/null; then
+	echo -e "\e[1;31mInstalling ..."$transparent""
+	xterm $HOLD -title "Installing Zenity" $TOPLEFTBIG -bg "#FFFFFF" -fg "#000000" -e apt-get install zenity
+else
+	echo -e "\e[1;32mOK!"$transparent""
+fi
+sleep 0.025
+##############################
+
+xterm $HOLD -title "Remove repositories"  -e python remove.py
