7f677d35db68c335dff6fb1136a585d7ce54be8f
Pedro Souza <programadorhacker8@gmail.com>
2016-10-07 12:42:53 -0300

    update pep8 (#171)

--- remove.py
+++ remove.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 
-import os
-import sys, traceback
+from os import remove
 
 infile = "/etc/apt/sources.list"
 outfile = "/etc/apt/sources.list"
 
-delete_list = ["# Kali linux repositories | Added by Katoolin\n", "deb http://http.kali.org/kali kali-rolling main contrib non-free\n","deb http://repo.kali.org/kali kali-bleeding-edge main\n"]
+delete_list = ["# Kali linux repositories | Added by Katoolin\n",
+               "deb http: // http.kali.org / kali kali - rolling main contrib non - free\n",
+               "deb http://repo.kali.org/kali kali-bleeding-edge main\n"]
+
 delete_list = ["deb http://ftp.de.debian.org/debian/ jessie main contrib"]
 fin = open(infile)
-os.remove("/etc/apt/sources.list")
+remove("/etc/apt/sources.list")
 fout = open(outfile, "w+")
 for line in fin:
     for word in delete_list:
@@ -18,4 +20,4 @@ for line in fin:
 fin.close()
 fout.close()
 
-print "\033[1;31mDONE!  \033[1;m"
+print("\033[1;31mDONE!  \033[1;m")
