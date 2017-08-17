82447ba763aeb00d356508470cb899f55319ce52
Matias Barcenas <MPX.4132@Gmail.com>
2017-08-17 17:45:48 -0500

    Suppressed grep file not found error.

--- lib/InterfaceUtils.sh
+++ lib/InterfaceUtils.sh
@@ -12,7 +12,7 @@ function interface_list_wireless() {
 	interface_list_all
 	local __interface_list_wireless__candidate
 	for __interface_list_wireless__candidate in "${InterfaceListAll[@]}"; do
-		if grep -q "DEVTYPE=wlan" /sys/class/net/$__interface_list_wireless__candidate/uevent 2> /dev/null
+		if grep -qs "DEVTYPE=wlan" /sys/class/net/$__interface_list_wireless__candidate/uevent
 		then InterfaceListWireless+=("$__interface_list_wireless__candidate")
 		fi
 	done
