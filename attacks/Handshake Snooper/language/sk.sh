1a93550f2dce090fb672ef2135bfe169b2ae1896
c0mrade69 <34037644+c0mrade69@users.noreply.github.com>
2017-11-28 19:17:26 +0100

    Create sk.sh
    Handshake Snopper slovak language

---
+++ attacks/Handshake Snooper/language/sk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# identifier: Handshake Snopper
+# description: Získa WPA/WPA2 kryptované 'hashes'.
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperMethodQuery="Vyberte metódu získania 'handshake'"
+HandshakeSnooperMonitorMethodOption="Monitor (${CYel}passive$CClr)"
+HandshakeSnooperAireplayMethodOption="aireplay-ng deauthentication (${CRed}aggressive$CClr)"
+HandshakeSnooperMdk3MethodOption="mdk3 deauthentication (${CRed}aggressive, not recommended$CClr)"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# FLUXSCRIPT END 
