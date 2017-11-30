ac88fecde84d24fadbcf5e9695682f44df239acb
Matias Barcenas <MPX.4132@Gmail.com>
2017-11-29 23:28:29 -0600

    Added new globals to language scripts.

--- attacks/Handshake Snooper/language/cs.sh
+++ attacks/Handshake Snooper/language/cs.sh
@@ -6,7 +6,16 @@
 HandshakeSnooperMethodQuery="Select a method of handshake retrieval"
 HandshakeSnooperMonitorMethodOption="Monitor (${CYel}passive$CClr)"
 HandshakeSnooperAireplayMethodOption="aireplay-ng deauthentication (${CRed}aggressive$CClr)"
-HandshakeSnooperMdk3MethodOption="mdk3 deauthentication (${CRed}aggressive, not recommended$CClr)"
+HandshakeSnooperMdk3MethodOption="mdk3 deauthentication (${CRed}aggressive$CClr)"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperVerifierIntervalQuery="How often should the verifier check for a handshake?"
+HandshakeSnooperVerifierInterval10SOption="Every 10 seconds (${CYel}fast systems only${CClr})."
+HandshakeSnooperVerifierInterval30SOption="Every 30 seconds (${CGrn}recommended${CClr})."
+HandshakeSnooperVerifierInterval90SOption="Every 90 seconds."
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperVerifierSynchronicityQuery="How should verification occur?"
+HandshakeSnooperVerifierSynchronicityAsynchronousOption="Asynchronously (${CYel}fast systems only${CClr})."
+HandshakeSnooperVerifierSynchronicitySynchronousOption="Synchronously."
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # FLUXSCRIPT END
--- attacks/Handshake Snooper/language/tk.sh
+++ attacks/Handshake Snooper/language/tk.sh
@@ -6,7 +6,16 @@
 HandshakeSnooperMethodQuery="Tokalasma edinme methodu seciniz"
 HandshakeSnooperMonitorMethodOption="Monitor (${CYel}pasif$CClr)"
 HandshakeSnooperAireplayMethodOption="aireplay-ng deauthentication (${CRed}agresif$CClr)"
-HandshakeSnooperMdk3MethodOption="mdk3 deauthentication (${CRed}agresif, onerilmez$CClr)"
+HandshakeSnooperMdk3MethodOption="mdk3 deauthentication (${CRed}agresif$CClr)"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperVerifierIntervalQuery="How often should the verifier check for a handshake?"
+HandshakeSnooperVerifierInterval10SOption="Every 10 seconds (${CYel}fast systems only${CClr})."
+HandshakeSnooperVerifierInterval30SOption="Every 30 seconds (${CGrn}recommended${CClr})."
+HandshakeSnooperVerifierInterval90SOption="Every 90 seconds."
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperVerifierSynchronicityQuery="How should verification occur?"
+HandshakeSnooperVerifierSynchronicityAsynchronousOption="Asynchronously (${CYel}fast systems only${CClr})."
+HandshakeSnooperVerifierSynchronicitySynchronousOption="Synchronously."
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # FLUXSCRIPT END
