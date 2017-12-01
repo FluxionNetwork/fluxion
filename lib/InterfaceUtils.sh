ead90d1f5b36974a3482e63b6765d8e20a3ac232
Matias Barcenas <MPX.4132@Gmail.com>
2017-11-30 20:58:54 -0600

    Fixed minor prompt styling issue.

--- lib/InterfaceUtils.sh
+++ lib/InterfaceUtils.sh
@@ -178,7 +178,7 @@ function interface_prompt() {
 	done
 
 	# The following conditional is required since io_query_format_fields
-	# only considers the the size of the first parameter, available color. 
+	# only considers the the size of the first parameter, available color.
 	if [ "$6" ]; then # Add alternative choices
 		__interface_prompt__ifAvailable+=("${!3}")
 		__interface_prompt__ifAvailableInfo+=("${!4}")
@@ -192,7 +192,7 @@ function interface_prompt() {
 		InterfacePromptWISelectedState="[+]" # It passed the condition, it must be +
 		InterfacePromptWISelectedInfo="${__interface_prompt__ifAvailableInfo[0]}"
     else
-		format_apply_autosize "$CRed[$CYel%1d$CRed]%b %-8b %3s %-*.*s\n"
+		format_apply_autosize "$CRed[$CYel%1d$CRed]%b %-8b %3s$CClr %-*.*s\n"
 		io_query_format_fields "$1" "$FormatApplyAutosize" \
 		__interface_prompt__ifAvailableColor[@] __interface_prompt__ifAvailable[@] \
 		__interface_prompt__ifAvailableState[@] __interface_prompt__ifAvailableInfo[@]
