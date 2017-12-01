f078363e80d5eb7c3e0e8e79c77f359a3a95dddd
Matias Barcenas <MPX.4132@Gmail.com>
2017-12-01 03:35:54 -0600

    Tweaked, fixed, and updated Handshake Snooper.
    Removed 10 seconds verification interval to allow deauths more down time.
    Added 60 seconds verification interval to allow for longer snooping times.
    Added synchronicity recommendation, favoring sychronous to prevent issues.
    Updated language files to reflect interval changes & recommendations.
    Fixed a bug caused by outdated code preventing returning from certain point.
    Tweaked the sequence, and timing of deauthenticators to improve performance.

--- attacks/Handshake Snooper/language/en.sh
+++ attacks/Handshake Snooper/language/en.sh
@@ -9,13 +9,13 @@ HandshakeSnooperAireplayMethodOption="aireplay-ng deauthentication (${CRed}aggre
 HandshakeSnooperMdk3MethodOption="mdk3 deauthentication (${CRed}aggressive$CClr)"
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 HandshakeSnooperVerifierIntervalQuery="How often should the verifier check for a handshake?"
-HandshakeSnooperVerifierInterval10SOption="Every 10 seconds (${CYel}fast systems only${CClr})."
 HandshakeSnooperVerifierInterval30SOption="Every 30 seconds (${CGrn}recommended${CClr})."
+HandshakeSnooperVerifierInterval60SOption="Every 60 seconds."
 HandshakeSnooperVerifierInterval90SOption="Every 90 seconds."
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 HandshakeSnooperVerifierSynchronicityQuery="How should verification occur?"
 HandshakeSnooperVerifierSynchronicityAsynchronousOption="Asynchronously (${CYel}fast systems only${CClr})."
-HandshakeSnooperVerifierSynchronicitySynchronousOption="Synchronously."
+HandshakeSnooperVerifierSynchronicitySynchronousOption="Synchronously (${CGrn}recommended${CClr})."
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 HandshakeSnooperStartingArbiterNotice="${CCyn}Handshake Snooper$CClr arbiter daemon running."
 HandshakeSnooperSnoopingForNSecondsNotice="Snooping for \$HANDSHAKEVerifierInterval seconds."
@@ -23,7 +23,7 @@ HandshakeSnooperStoppingForVerifierNotice="Stopping snooper & checking for hashe
 HandshakeSnooperSearchingForHashesNotice="Searching for hashes in the capture file."
 HandshakeSnooperArbiterAbortedWarning="${CYel}Aborted${CClr}: The operation's been aborted, no valid hash was found."
 HandshakeSnooperArbiterSuccededNotice="${CGrn}Success${CClr}: A valid hash was detected and saved to fluxion's database."
-HandshakeSnooperArbiterCompletedTip="${CCyn}Handshake Snooper$CYel attack completed, close this window and start another attack.$CClr"
+HandshakeSnooperArbiterCompletedTip="${CBCyn}Handshake Snooper$CBYel attack completed, close this window and start another attack.$CClr"
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 # FLUXSCRIPT END
--- attacks/Handshake Snooper/language/tk.sh
+++ attacks/Handshake Snooper/language/tk.sh
@@ -9,13 +9,13 @@ HandshakeSnooperAireplayMethodOption="aireplay-ng deauthentication (${CRed}agres
 HandshakeSnooperMdk3MethodOption="mdk3 deauthentication (${CRed}agresif$CClr)"
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 HandshakeSnooperVerifierIntervalQuery="How often should the verifier check for a handshake?"
-HandshakeSnooperVerifierInterval10SOption="Every 10 seconds (${CYel}fast systems only${CClr})."
 HandshakeSnooperVerifierInterval30SOption="Every 30 seconds (${CGrn}recommended${CClr})."
+HandshakeSnooperVerifierInterval60SOption="Every 60 seconds."
 HandshakeSnooperVerifierInterval90SOption="Every 90 seconds."
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 HandshakeSnooperVerifierSynchronicityQuery="How should verification occur?"
 HandshakeSnooperVerifierSynchronicityAsynchronousOption="Asynchronously (${CYel}fast systems only${CClr})."
-HandshakeSnooperVerifierSynchronicitySynchronousOption="Synchronously."
+HandshakeSnooperVerifierSynchronicitySynchronousOption="Synchronously (${CGrn}recommended${CClr})."
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 HandshakeSnooperStartingArbiterNotice="${CCyn}Handshake Snooper$CClr arbiter daemon running."
 HandshakeSnooperSnoopingForNSecondsNotice="Snooping for \$HANDSHAKEVerifierInterval seconds."
