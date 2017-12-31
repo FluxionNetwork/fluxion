7beec1cf25682aa1f7094b5620c7245971d530c9
Matias Barcenas <MPX.4132@Gmail.com>
2017-12-30 23:49:20 -0600

    Added unstaged files from past commit.

---
+++ attacks/Captive Portal/language/pt-br.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# identifier: Captive Portal
+# description: Creates an "evil twin" access point.
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+CaptivePortalInterfaceQuery="Select an interface for the captive portal."
+CaptivePortalStartingInterfaceNotice="Starting captive portal interface..."
+CaptivePortalCannotStartInterfaceError="${CRed}Unable to start captive portal interface$CClr, returning!"
+CaptivePortalStartedInterfaceNotice="${CGrn}Success${CClr}, captive portal interface ready!"
+CaptivePortalStaringAPServiceNotice="Starting Captive Portal access point service..."
+CaptivePortalStaringAPRoutesNotice="Starting Captive Portal access point routes..."
+CaptivePortalStartingDHCPServiceNotice="Starting access point DHCP service as daemon..."
+CaptivePortalStartingDNSServiceNotice="Starting access point DNS service as daemon..."
+CaptivePortalStartingWebServiceNotice="Starting access point captive portal as daemon..."
+CaptivePortalStartingJammerServiceNotice="Starting access point jammer as daemon..."
+CaptivePortalStartingAuthenticatorServiceNotice="Starting authenticator script..."
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+CaptivePortalVerificationMethodQuery="Select a password verification method"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+CaptivePortalCertificateSourceQuery="Select SSL certificate source for captive portal."
+CaptivePortalCertificateSourceGenerateOption="Create an SSL certificate"
+CaptivePortalCertificateSourceRescanOption="Detect SSL certificate (${CClr}search again$CGry)"
+CaptivePortalCertificateSourceDisabledOption="None (${CYel}disable SSL$CGry)"
+CaptivePortalUIQuery="Select a captive portal interface for the rogue network."
+CaptivePortalGenericInterfaceOption="Generic Portal"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+CaptivePortalConnectivityQuery="Select an internet connectivity type for the rogue network."
+CaptivePortalConnectivityDisconnectedOption="disconnected (${CGrn}recommended$CClr)"
+CaptivePortalConnectivityEmulatedOption="emulated"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# FLUXSCRIPT END
---
+++ attacks/Handshake Snooper/language/pt-br.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# identifier: Handshake Snopper
+# description: Acquires WPA/WPA2 encryption hashes.
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperMethodQuery="Select a method of handshake retrieval"
+HandshakeSnooperMonitorMethodOption="Monitor (${CYel}passive$CClr)"
+HandshakeSnooperAireplayMethodOption="aireplay-ng deauthentication (${CRed}aggressive$CClr)"
+HandshakeSnooperMdk3MethodOption="mdk3 deauthentication (${CRed}aggressive$CClr)"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperVerifierIntervalQuery="How often should the verifier check for a handshake?"
+HandshakeSnooperVerifierInterval30SOption="Every 30 seconds (${CGrn}recommended${CClr})."
+HandshakeSnooperVerifierInterval60SOption="Every 60 seconds."
+HandshakeSnooperVerifierInterval90SOption="Every 90 seconds."
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperVerifierSynchronicityQuery="How should verification occur?"
+HandshakeSnooperVerifierSynchronicityAsynchronousOption="Asynchronously (${CYel}fast systems only${CClr})."
+HandshakeSnooperVerifierSynchronicitySynchronousOption="Synchronously (${CGrn}recommended${CClr})."
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+HandshakeSnooperStartingArbiterNotice="${CCyn}Handshake Snooper$CClr arbiter daemon running."
+HandshakeSnooperSnoopingForNSecondsNotice="Snooping for \$HANDSHAKEVerifierInterval seconds."
+HandshakeSnooperStoppingForVerifierNotice="Stopping snooper & checking for hashes."
+HandshakeSnooperSearchingForHashesNotice="Searching for hashes in the capture file."
+HandshakeSnooperArbiterAbortedWarning="${CYel}Aborted${CClr}: The operation's been aborted, no valid hash was found."
+HandshakeSnooperArbiterSuccededNotice="${CGrn}Success${CClr}: A valid hash was detected and saved to fluxion's database."
+HandshakeSnooperArbiterCompletedTip="${CBCyn}Handshake Snooper$CBYel attack completed, close this window and start another attack.$CClr"
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# FLUXSCRIPT END
