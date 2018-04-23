21ece945d6364f28a9e097c1bb9b82e7989bb58f
Matias Barcenas <MPX.4132@Gmail.com>
2018-04-23 14:30:52 -0500

    Updated Captive Portal language files.

--- attacks/Captive Portal/language/ru.sh
+++ attacks/Captive Portal/language/ru.sh
@@ -2,17 +2,10 @@
 # identifier: Captive Portal
 # description: Создаёт точку доступа "Злой Двойник".
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 CaptivePortalJammerInterfaceQuery="Выберите интерфейс для глушения."
 CaptivePortalAccessPointInterfaceQuery="Выберите интерфейс для точки доступа."
-
-CaptivePortalAPServiceQuery="Выберите службу точки доступа"
-CaptivePortalAPServiceHostapdOption="Rogue AP - hostapd (${CGrn}рекомендуется$CClr)"
-CaptivePortalAPServiceAirbaseOption="Rogue AP - airbase-ng (${CYel}медленная$CClr)"
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-CaptivePortalInterfaceQuery="Выберите интерфейс для перехватывающего портала (captive portal)."
-CaptivePortalStartingInterfaceNotice="Запуск интерфейса перехватывающего портала..."
 CaptivePortalCannotStartInterfaceError="${CRed}Не получается запустить перехватывающий портал interface$CClr, возврат!"
-CaptivePortalStartedInterfaceNotice="${CGrn}Успех${CClr}, интерфейс перехватывающего портала готов!"
 CaptivePortalStaringAPServiceNotice="Запуск службы точки доступа с Перехватывающим Порталом..."
 CaptivePortalStaringAPRoutesNotice="Запуск маршрутизации точки доступа с Перехватывающим Порталом..."
 CaptivePortalStartingDHCPServiceNotice="Запуск службы DHCP точки доступа в качестве демона..."
@@ -20,6 +13,10 @@ CaptivePortalStartingDNSServiceNotice="Запуск службы DNS точки 
 CaptivePortalStartingWebServiceNotice="Запуск точки доступа с Перехватывающим Порталом в качестве демона..."
 CaptivePortalStartingJammerServiceNotice="Запуск глушителя точки доступа в качестве демона..."
 CaptivePortalStartingAuthenticatorServiceNotice="Запуск скрипта аутентификации..."
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+CaptivePortalAPServiceQuery="Выберите службу точки доступа"
+CaptivePortalAPServiceHostapdOption="Rogue AP - hostapd (${CGrn}рекомендуется$CClr)"
+CaptivePortalAPServiceAirbaseOption="Rogue AP - airbase-ng (${CYel}медленная$CClr)"
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 CaptivePortalVerificationMethodQuery="Выбор метода верификации пароля"
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
