f1650e0f0764da990d4482075ea1a1c428815b45
Matias Barcenas <MPX.4132@Gmail.com>
2018-04-24 19:36:09 -0500

    Updated portals & authenticator.

--- attacks/Captive Portal/lib/update.php
+++ attacks/Captive Portal/lib/update.php
@@ -2,7 +2,8 @@
 	require_once("authenticator.php");
 
 	switch ($candidate_code) {
-		# case "1": echo "false"; break;
-		case "2": echo "true"; break;
-		default: echo "false"; break;
+		# case "1": echo ""; break;
+		case "2": echo "authenticated"; break;
+		# default: echo ""; break;
+		default: break;
 	}
