676f5a9b7b86c143ba598f276aa0c3976ec3ac14
Matias Barcenas <MPX.4132@Gmail.com>
2018-05-04 12:52:53 -0500

    Changed recommened handshake verfier.

--- language/ru.sh
+++ language/ru.sh
@@ -61,9 +61,9 @@ FLUXIONUseFoundHashQuery="Вы хотите использовать этот ф
 FLUXIONUseFoundHashOption="Использовать найденное рукопожатие"
 FLUXIONSpecifyHashPathOption="Укажите путь к рукопожатию"
 FLUXIONHashVerificationMethodQuery="Выберите метод проверки рукопожатия"
-FLUXIONHashVerificationMethodPyritOption="проверка с помощью pyrit (${CGrn}рекомендуется$CClr)"
+FLUXIONHashVerificationMethodPyritOption="проверка с помощью pyrit"
 FLUXIONHashVerificationMethodAircrackOption="проверка с помощью aircrack-ng (${CYel}ненадёжная$CClr)"
-FLUXIONHashVerificationMethodCowpattyOption="cowpatty verification"
+FLUXIONHashVerificationMethodCowpattyOption="проверка с помощью cowpatty (${CGrn}рекомендуется$CClr)"
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 FLUXIONAttackQuery="Выбор беспроводной атаки для точки доступа"
 FLUXIONAttackInProgressNotice="${CCyn}\$FluxionAttack$CClr идёт атака..."
