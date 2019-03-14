936cf4d7b33e98d8c1686312e5872dd2e0670d6f
Matias Barcenas <MPX.4132@Gmail.com>
2019-03-14 00:38:42 -0500

    Merged repositories.

--- fluxion_origins/attacks/Captive Portal/lib/connectivity responses/Google/generate_204.php
+++
@@ -1,2 +0,0 @@
-<?php
-	header("HTTP/1.0 204 No Content");
--- fluxion_origins/attacks/Captive Portal/lib/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once("authenticator.php");
-
-	switch ($candidate_code) {
-		# case "1": echo ""; break;
-		case "2": echo "authenticated"; break;
-		# default: echo ""; break;
-		default: break;
-	}
