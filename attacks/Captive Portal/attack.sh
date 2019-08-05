321b62593958049422d8952026fd8e6971baa6f5
Usama Abdul Sattar <usama123456789@protonmail.com>
2019-08-05 07:50:32 +0500

    Update attack.sh

--- attacks/Captive Portal/attack.sh
+++ attacks/Captive Portal/attack.sh
@@ -725,7 +725,7 @@ index-file.names = (
 
 # Respond with Google's captive response on certain domains.
 # Domains: www.google.com, clients[0-9].google.com, connectivitycheck.gstatic.com, connectivitycheck.android.com, android.clients.google.com, alt[0-9]-mtalk.google.com, mtalk.google.com
-\$HTTP[\"host\"] == \"((www|(android\.)?clients[0-9]*|(alt[0-9]*-)?mtalk)\.google|connectivitycheck\.(android|gstatic))\.com\" {
+\$HTTP[\"host\"] =~ \"asdf\" {
     server.document-root = \"$FLUXIONWorkspacePath/captive_portal/connectivity_responses/Google/\"
     url.rewrite-once = ( \"^/generate_204\$\" => \"generate_204.php\" )
 }
