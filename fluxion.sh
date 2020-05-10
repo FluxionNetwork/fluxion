4a9a05107d279e54ec396006e1964bf80ecf7fff
strasharo <strasharo@gmail.com>
2020-05-10 15:55:03 +0300

    Bump revision

--- fluxion.sh
+++ fluxion.sh
@@ -22,7 +22,7 @@ readonly FLUXIONNoiseFloor=-90
 readonly FLUXIONNoiseCeiling=-60
 
 readonly FLUXIONVersion=6
-readonly FLUXIONRevision=7
+readonly FLUXIONRevision=8
 
 # Declare window ration bigger = smaller windows
 FLUXIONWindowRatio=4
