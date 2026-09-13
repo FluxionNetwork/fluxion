f1d69c2087d2768b304bdde954bc687c57a0b872
strasharo <3337997+strasharo@users.noreply.github.com>
2026-09-13 14:24:28 +0300

    Bump revision to 33

--- fluxion.sh
+++ fluxion.sh
@@ -28,7 +28,7 @@ readonly FLUXIONNoiseFloor=-90
 readonly FLUXIONNoiseCeiling=-60
 
 readonly FLUXIONVersion=6
-readonly FLUXIONRevision=32
+readonly FLUXIONRevision=33
 
 # Declare window ration bigger = smaller windows
 FLUXIONWindowRatio=4
